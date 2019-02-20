aee9d16784caaefd6c32c80ceffa626ceb73a747
Aaron Snipes <aaron.snipes@dudesolutions.com>
2019-02-20 09:56:33 -0500

    added pic and can deploy now

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Hey mafacka" />
+    <HelloWorld msg="Hey mafacka, i deployed" />
+    <div :style="{ backgroundImage: `../assets/145290-OTJO7F-910.jpg` }"></div>
   </div>
 </template>
 
@@ -16,3 +17,22 @@ export default {
   }
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.home{
+  background-image: url("~@/assets/145290-OTJO7F-910.jpg");
+  margin: 0 auto;
+  width: 95%;
+  position: relative;
+  margin-top: 0;
+  margin-right: auto;
+  margin-bottom: 0;
+  margin-left: auto;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+</style>
