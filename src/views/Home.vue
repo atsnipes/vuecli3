800d7410992c944b22b2b96982bf73d8e8c575cd
Aaron Snipes <aaron.snipes@dudesolutions.com>
2019-02-21 23:34:24 -0500

    Add state abbreviator to home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Hey mafacka, i deployed" />
+    <img alt="Vue logo" src="../assets/logo.png">
+    <StateAbbreviator></StateAbbreviator>
+    <HelloWorld msg="Hey mafacka, i deployed"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "../components/HelloWorld.vue";
+import StateAbbreviator from "../components/StateAbbreviator.vue";
+import BeerApiClient from "../api/beerApiClient.js";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    StateAbbreviator
+  },
+  props: {
+    beerPrice: String
+  },
+  async mounted() {
+    const result = await BeerApiClient.getBreweriesByState("North Carolina");
+    console.log(result);
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.home{
+.home {
   background-image: url("~@/assets/145290-OTJO7F-910.jpg");
   margin: 0 auto;
   width: 95%;
@@ -33,5 +44,4 @@ export default {
   -o-background-size: cover;
   background-size: cover;
 }
-
 </style>
